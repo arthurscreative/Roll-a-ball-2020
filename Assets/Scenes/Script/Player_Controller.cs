a8f6806dc6b4e530babc534233a050b13db9a9f9
arthurscreative <arthur.silva@rutgers.edu>
2020-01-29 01:52:56 -0500

    Script Written and Saved
    Mistake at first but fixed it

--- Assets/Scenes/Script/Player_Controller.cs
+++ Assets/Scenes/Script/Player_Controller.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Rigidbody rb;
+
+    void Start ()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() { 
+
+     
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3 (moveHorizontal , 0.0f, moveVertical);
+        rb.AddForce(movement);
+
     }
 }
